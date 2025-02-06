760e40e29553cd30e671f0e8ff5185272d5e04ea
dansheng <73804513+StupidBoy558@users.noreply.github.com>
2025-02-06 21:02:30 +0800

    添加注释

--- src/main/java/com/geektime/ratelimiter/rule/package-info.java
+++ src/main/java/com/geektime/ratelimiter/rule/package-info.java
@@ -1,4 +1,10 @@
+package com.geektime.ratelimiter.rule;
+
 /**
  * 限流规则相关类
+ * 
+ * 包含以下主要类：
+ * - RateLimitRule: 限流规则管理类
+ * - ApiLimit: API限流规则配置类
+ * - RuleConfig: 限流规则配置管理类
  */
-package com.geektime.ratelimiter.rule;
